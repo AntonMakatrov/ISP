58ffdf542a05723309b93acc5f29b9c25645dd61
AntonMakatrov <74259724+AntonMakatrov@users.noreply.github.com>
2021-05-07 16:22:25 +0300

    Add files via upload

--- lab7/lab7/Program.cs
+++ lab7/lab7/Program.cs
@@ -12,20 +12,27 @@ namespace Lab7
         {
             RationalNumber n1 = new RationalNumber(1, 2);
             RationalNumber n2 = new RationalNumber(3, 4);
+            
             RationalNumber n = n1 + n2;
             Console.WriteLine($"{n1} + {n2} = " + n);
+            
             n = n1 - n2;
             Console.WriteLine($"{n1} - {n2} = " + n.ToString("."));
+            
             n = n1 * n2;
             Console.WriteLine($"{n1} * {n2} = " + n);
+            
             n = n1 / n2;
             Console.WriteLine($"{n1} / {n2} = " + n);
+            
             Console.WriteLine($"{n1} > {n2} " + (n1 > n2));
-            Console.WriteLine($"{n1} < {n2} " + (n1 < n2));
+            Console.WriteLine($"{n1} < {n2} " + (n1 < n2));            
             Console.WriteLine($"{n1} == {n2} " + (n1 == n2));
             Console.WriteLine($"{n1} != {n2} " + (n1 != n2));
+            
             Console.WriteLine("parse -1.3 " + RationalNumber.Parse("-1.3"));
             Console.WriteLine("parse -1/3 " + RationalNumber.Parse("-1/3"));
+            
             n.Reduce();
             Console.WriteLine("reduce 4/6 " + n);
             Console.WriteLine($"(int){n} " + (int)n);
